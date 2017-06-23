056575f30d4062fcafbeb4824b0c48ba15681a70
Dan Lofaro <dan@danlofaro.com>
2017-06-23 16:49:11 -0400

    finished time to send to eva

--- light-look-test/light_touch_test.py
+++ light-look-test/light_touch_test.py
@@ -11,21 +11,47 @@ if __name__ == '__main__':
     # ti.init( port, timeout)
     # Return: Null
     test.init('/dev/ttyACM0',5)
-    
+
+    # Set log filename (optional, default = look_light_test.log)
+    print test.log_name('look_light_test3.log')    
     while (True):
+
+      # Get random right left values
+      # Return: 'right' or 'left'
+      robot_look = test.random_right_left()
+      bulb_light = test.random_right_left()
+
+      # get random delay from head
+      robot_head_delay  = test.random_delay(0.1, 0.4)
+
+      # reset delay time in seconds
+      robot_reset_delay = test.random_delay(4.0, 7.0)
+
       # input:   Side Robot Looks ['left', 'right'],
       #          Side Bulb Lights ['left', 'right'],
-      #          Delay of robot look (in sec) (Negative values only 
+      #          Time robot looks before light turns on (in sec) ** Optional **
+      #          Time robot pauses during reset (in sec) ** Optional **
       #
       # return ( Side Robot Looked ['left', 'right'],
       #          Side Human Picked ['left', 'right'],
       #          Side Bulb Lit On  ['left', 'right'],
+      #          Time robot looks before light turns on (in sec) 
+      #          Time robot pauses during reset (in sec) 
       #          Change in time between light going on and human touch (sec)
       #         )
-      out = test.run('right', 'left')
-      test.log(out)
-      print out
+      out = test.run(robot_look, bulb_light, robot_head_delay, robot_reset_delay)
 
+
+      # Log the output
+      # Log format:
+      # Time of log sing "beginning of time" in seconds
+      # Side Robot Looked
+      # Side Human Touched
+      # Side Light Lit
+      # When robot started to move it's head in reference to when light turned on in seconds
+      # Difference in time between when the light turned on and the human touched the input in seconds
+      # NEW LINE
+      test.log(out)
 
     # Stops system
     test.stop()
--- light-look-test/test_macros.py
+++ light-look-test/test_macros.py
@@ -2,6 +2,7 @@ import meka_ach as ma
 import touch_input as ti
 import light_look_test_macros as look
 import time
+import random
 
 the_file_name = 'look_light_test.log'
 
@@ -30,12 +31,13 @@ def reset(t = None):
 
 def run(side_robot, side_light,robot_pre = None, hold_time = None ):
   if(hold_time == None):
-    reset(5.0)
-  else:
-    reset(hold_time)
+    hold_time = 5.0
+  reset(hold_time)
 
   if(robot_pre == None):
     robot_pre = -0.2
+  else:
+    robot_pre = -1.0 * robot_pre
 
   human = 'F'
   robot = 'F'
@@ -59,13 +61,41 @@ def run(side_robot, side_light,robot_pre = None, hold_time = None ):
     side_human = 'left'
 
   dt = b[1] - a
-  return (side_robot, side_human, side_light, dt)
+  return (side_robot, side_human, side_light, robot_pre, hold_time, dt)
      
 
 def log( from_run):
+  # Log format:
+  # Time of log sing "beginning of time" in seconds
+  # Side Robot Looked
+  # Side Human Touched
+  # Side Light Lit
+  # When robot started to move it's head in reference to when light turned on in seconds
+  # time robot pauses during reset in seconds
+  # Difference in time between when the light turned on and the human touched the input in seconds
+  # NEW LINE
   global the_file_name
   target = open(the_file_name, 'a')
 
   t = time.time()
-  target.write(str(t) + " " + from_run[0] + " " + from_run[1] + " " + from_run[2] + " " + str(from_run[3])+ '\n')  
-  target.close() 
+  target.write(str(t) + " " + from_run[0] + " " + from_run[1] + " " + from_run[2] + " " + str(from_run[3])+ " " +str(from_run[4]) + " " + str(from_run[5]) + '\n')  
+  target.close()
+
+def random_delay(t0, t1):
+  a = random.uniform(t0, t1)
+  return a
+
+def random_right_left():
+  a = random.random()
+  if(a < 0.5): 
+    return 'left'
+  return 'right' 
+
+
+
+def log_name(log_name = None):
+  global the_file_name
+  if(log_name == None):
+    return the_file_name
+  the_file_name = log_name
+  return the_file_name
